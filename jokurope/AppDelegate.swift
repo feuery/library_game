b6742ccdb969a446aa7509d9641e491ac41973c7
Ilpo Lehtinen <feuer@feuerx.net>
2022-12-24 14:58:17 +0200

    Initial Commit

---
+++ jokurope/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  jokurope
+//
+//  Created by Ilpo Lehtinen on 24.12.2022.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
