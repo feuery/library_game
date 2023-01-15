d1fc642d3d806fccb37276df8b2d677938d7302f
Ilpo Lehtinen <feuer@feuerx.net>
2023-01-15 15:21:45 +0200

    Moving the player works correctly regarding the animations

--- jokurope/GameScene.swift
+++ jokurope/GameScene.swift
@@ -38,32 +38,86 @@ class GameScene: SKScene {
     var animations: Dictionary<String, SKAction> {
         return textures.mapValues{ anims in SKAction.animate(with: anims, timePerFrame: 0.5)}
     }
+    
+    // var dude: SKSpriteNode{
+    //     return SKSpriteNode(texture: textures["right"]!.first)
+    // }
         
     override func didMove(to view: SKView)  {
         
-        let animation = SKAction.animate(with: textures["right"]!, timePerFrame: 0.1)
+        // let animation = SKAction.animate(with: textures["right"]!, timePerFrame: 0.1)
+       // assert(textures.allSatisfy{ !$1.isEmpty && $1.count > 0})
         //animations["right"]
         
-        let dude = SKSpriteNode(texture: textures["right"]!.first)
         
-        dude.position = CGPoint(x: 100, y: 100)
-        dude.run(SKAction.repeatForever(animation), withKey: "down_anim")
-        self.addChild(dude)
+        
+        // // dude.position = CGPoint(x: 100, y: 100)
+        // if let dude = self.dude {
+        //     dude.run(SKAction.repeatForever(animation), withKey: "down_anim")
+        // }
+        // self.addChild(dude)
+    }
+    
+    var dude: SKSpriteNode? {
+        return childNode(withName: "dude") as? SKSpriteNode
     }
 
-    func update(currentTime: TimeInterval) {
+    private var oldDirection = ""
+
+    override func update(_ currentTime: TimeInterval) {
+        
         guard let direction = (leftPressed ? "left":
-                                 rightPressed ? "right":
-                                 upPressed ? "up":
-                                 downPressed ? "down": nil) else { return}
+                                rightPressed ? "right":
+                                upPressed ? "up":
+                                downPressed ? "down": nil) else {
+            //print("Direction is nil")
+            return
+        }
         
+        guard let dude = dude else {
+            print("Dude is nil")
+            return
+        }
+
+        let duration = 1.0
+        let vect = direction == "left" ?
+        CGVector.init(dx: -1.0, dy: 0):
+        direction == "right" ?
+        CGVector(dx: 1, dy: 0):
+        direction == "up" ?
+        CGVector(dx:0, dy: 1):
+        direction == "down" ?
+        CGVector(dx: 0, dy: -1):
+        nil
         
+        if let vector = vect {
+            let animation = SKAction.repeatForever(SKAction.animate(with: textures[direction]!, timePerFrame: 0.1))
+            let moveAction = SKAction.move(by: vector,
+                                           duration: duration)
+            
+            let action = direction == oldDirection ?
+              moveAction :
+              SKAction.group([moveAction, animation])
+            
+            oldDirection = direction
+            
+            print("direction: " + vector.debugDescription)
+            
+            dude.run(action)
+        }
     }
     
     var leftPressed: Bool = false
     var rightPressed: Bool = false
     var upPressed: Bool = false
     var downPressed: Bool = false
+
+    private var allKeys: [Bool] {
+        return [leftPressed,
+                rightPressed,
+                upPressed,
+                downPressed]
+    }
 
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
@@ -92,6 +146,17 @@ class GameScene: SKScene {
             downPressed = false
         default:
             break
+        }
+        
+        let allKeysUp = allKeys.map(!).reduce(true) {$0 && $1}
+        
+        if allKeysUp {
+            guard let dude = dude else {
+                print("Dude is nil")
+                return
+            }
+            print("Not pushing anything, removing all actions")
+            dude.removeAllActions()
         }
     }
 }
